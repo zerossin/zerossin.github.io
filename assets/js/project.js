ca881a1bc354a91bd965053eb4f57ca58ad26e58
zerossin <koreaminsoo@naver.com>
2024-12-03 08:41:01 +0900

    네임변경

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,51 +1,51 @@
 const projectItems = [
     {
-        href: "https://github.com/mintsclover/room-classifier",
+        href: "https://github.com/zerossin/room-classifier",
         imgSrc: "assets/images/logo_indoor.png",
         title: "YOLOv5- room classifier",
     },
     {
-        href: "https://gitpage.mintsclover.com/js-rhythm-game/",
+        href: "https://gitpage.zerossin.com/js-rhythm-game/",
         imgSrc: "assets/images/js-rhythm-game.png",
         title: "리듬게임",
     },
     {
-        href: "https://github.com/mintsclover/Disa-Project",
+        href: "https://github.com/zerossin/Disa-Project",
         imgSrc: "assets/images/logo_disa_project.png",
         title: "DISA Project",
     },
     {
-        href: "https://gitpage.mintsclover.com/source-code-scanner/",
+        href: "https://gitpage.zerossin.com/source-code-scanner/",
         imgSrc: "assets/images/logo_source_code_scanner.jpeg",
         title: "Source Code Scanner",
     },
     {
-        href: "https://map.mintsclover.com/",
+        href: "https://map.zerossin.com/",
         imgSrc: "assets/images/logo_mc_map.png",
         title: "Map",
     },
     {
-        href: "https://github.com/mintsclover/python-korean-word-chain-game",
+        href: "https://github.com/zerossin/python-korean-word-chain-game",
         imgSrc: "assets/images/logo_word_chain_game.png",
         title: "Korean word chain game",
     },
     {
-        href: "https://github.com/mintsclover/youtube-music-downloader",
+        href: "https://github.com/zerossin/youtube-music-downloader",
         imgSrc: "assets/images/imsi.png",
         title: "youtube music downloader",
     },
     {
-        href: "https://github.com/mintsclover/guminai",
+        href: "https://github.com/zerossin/guminai",
         imgSrc: "assets/images/guminai.png",
         title: "guminai",
     },
     {
-        href: "https://github.com/mintsclover/database-project",
+        href: "https://github.com/zerossin/database-project",
         imgSrc: "assets/images/database-project.jpeg",
         title: "database project",
     },
     {
-        href: "https://github.com/mintsclover/image-converter",
+        href: "https://github.com/zerossin/image-converter",
         imgSrc: "assets/images/imsi.png",
         title: "image converter",
     },
